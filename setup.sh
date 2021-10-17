4f464061245007dd9c33d495e850b7207850346c
Ngo Cuong <77053331+ngocuong0105@users.noreply.github.com>
2021-10-17 15:16:07 +0300

    prepare deploy heroku

--- setup.sh
+++ setup.sh
@@ -5,5 +5,9 @@ echo "
 headless = true\n
 port = $PORT\n
 enableCORS = false\n
+[theme]
+base = 'light'\n
+primaryColor = '#ffcc2a'\n
+backgroundColor = '#f8f4d5'\n
 \n
 " > ~/.streamlit/config.toml

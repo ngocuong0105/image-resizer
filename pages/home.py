cb6df0e99a8a23d2817e306009dd01691acfbd50
Ngo Cuong <ngo.cuong@abv.bg>
2022-01-20 17:22:53 +0200

    format code with black

--- pages/home.py
+++ pages/home.py
@@ -1,9 +1,10 @@
-'''
+"""
 Streamlit home page
-'''
+"""
 import streamlit as st
 
-def show_title(title:str):
-    with open('static/title_style.css') as f:
-        title_html = f'<style>{f.read()}</style><h1>{title}</h1>'
-    st.markdown(title_html,unsafe_allow_html=True)
+
+def show_title(title: str):
+    with open("static/title_style.css") as f:
+        title_html = f"<style>{f.read()}</style><h1>{title}</h1>"
+    st.markdown(title_html, unsafe_allow_html=True)

9512255bf2d8d6d91fb7ccdba3fce589a522e1ec
Ngo Cuong <ngo.cuong@abv.bg>
2022-01-27 18:59:21 +0200

    format with black

--- imageResizer/test_resizeable_image.py
+++ imageResizer/test_resizeable_image.py
@@ -2,13 +2,14 @@ import unittest
 import sys
 from pages.resizeImage import open_file
 
+
 class TestImage(unittest.TestCase):
     def test_image(self):
         image = open_file()
         seam = image.best_seam()
 
         # Make sure the seam is of the appropriate length.
-        self.assertEqual(image.height, len(seam), 'Seam wrong size.')
+        self.assertEqual(image.height, len(seam), "Seam wrong size.")
 
         # Make sure the pixels in the seam are properly connected.
         # seam.sort(cmp=lambda x,y: (x[1]>y[1])-(x[1]<y[1])) # Sort by height.
@@ -19,6 +20,7 @@ class TestImage(unittest.TestCase):
         # Make sure the energy of the seam matches what we expect.
         # total = sum([image.energy(coord[0], coord[1]) for coord in seam])
         # self.assertEqual(total, expected_cost)
-            
-if __name__ == '__main__':
-    unittest.main(argv = sys.argv + ['--verbose'])
+
+
+if __name__ == "__main__":
+    unittest.main(argv=sys.argv + ["--verbose"])
